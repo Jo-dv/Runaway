dc94f8e638ef2abf53177463bec37523c3c738e7
ymkim0216 <qkslzl101216@naver.com>
2023-11-20 11:34:21 +0900

    init merge

--- front/final-project/src/components/notice/NoticePagination.vue
+++ front/final-project/src/components/notice/NoticePagination.vue
@@ -1,35 +1,35 @@
+<script setup>
+import { useNoticeStore } from '../../stores/noticeStore';
+const { noticeStore, startPageIndex, endPageIndex, prev, next } = useNoticeStore();
+const emit = defineEmits(['call-parent'])
+  const paginationChanged = (pageIndex) => {
+    console.log("paginationVue : paginationChanged : pageIndex : " + pageIndex );
+    emit('call-parent', pageIndex);
+  }
+</script>
 <template>
   <div class="col-lg-12 col-12">
-    <nav aria-label="Page navigation example">
+    <nav aria-label="Page navigation exsample">
       <ul class="pagination justify-content-center mb-0">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
+        <li v-if="prev" class="page-item">
+          <a class="page-link" href="#" aria-label="Previous" @click="paginationChanged(startPageIndex-1)">
             <span aria-hidden="true">Prev</span>
           </a>
         </li>
 
-        <li class="page-item active" aria-current="page">
-          <a class="page-link" href="#">1</a>
+        <li v-for="index in (endPageIndex-startPageIndex+1)" :key="index"
+        :class = "{
+          active:(startPageIndex+index-1==noticeStore.currentPageIndex)}"
+        class="page-item" aria-current="page">
+          <a class="page-link" href="#"
+          @click="paginationChanged(startPageIndex+index-1)"
+          >{{startPageIndex+index-1}}</a>
         </li>
 
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-
-        <li class="page-item">
-          <a class="page-link" href="#">4</a>
-        </li>
-
-        <li class="page-item">
-          <a class="page-link" href="#">5</a>
-        </li>
-
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
+        <li v-if ="next" class="page-item" >
+          <a class="page-link" href="#" aria-label="Next"
+          @click="paginationChanged(endPageIndex+1)"
+          >
             <span aria-hidden="true">Next</span>
           </a>
         </li>
@@ -37,3 +37,5 @@
     </nav>
   </div>
 </template>
+<style scoped>
+</style>
